544ff5e4eeb2d38fe8c5bd6d40b0a47a6c3577d5
Carl Angeles <CAngeles@academytrainee.com>
2021-01-06 01:13:14 +0000

    CA: Added dummy customer data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,7 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Fubuki', 'Shirakami');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Aqua', 'Minato');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Pekora', 'Usada');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Shion', 'Murasaki');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Ayame', 'Nakiri');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Haato', 'Akai');
+INSERT INTO `ims`.`customers` (`firstName`, `lastName`) VALUES ('Rion', 'Takamiya');
