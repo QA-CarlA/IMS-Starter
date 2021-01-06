39085c1e030bb96b653c0450dd6dbc223d85550c
Carl Angeles <CAngeles@academytrainee.com>
2021-01-06 14:05:16 +0000

    CA: Implemented Read function for orders and added dummy data

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -11,6 +12,7 @@ import java.util.Map.Entry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DBUtils;
 
@@ -19,9 +21,21 @@ public class OrderDAO implements Dao<Order>
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	@Override
-	public List<Order> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Order> readAll() 
+	{
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from orders");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
 	}
 	
 	public Order readLatest() 
--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -14,3 +14,26 @@ INSERT INTO `ims`.`items` (`itemName`, `itemPrice`, `itemStockCount`) VALUES ('B
 INSERT INTO `ims`.`items` (`itemName`, `itemPrice`, `itemStockCount`) VALUES ('Novel Vol.1', 4.99, 11);
 INSERT INTO `ims`.`items` (`itemName`, `itemPrice`, `itemStockCount`) VALUES ('Novel Vol.2', 4.99, 39);
 INSERT INTO `ims`.`items` (`itemName`, `itemPrice`, `itemStockCount`) VALUES ('Novel Vol.3', 4.99, 83);
+
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('1');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('2');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('3');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('4');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('5');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('6');
+INSERT INTO `ims`.`orders` (`customerID`) VALUES ('7');
+
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (1, 1, 2);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (1, 2, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (1, 4, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (2, 5, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (2, 6, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (3, 2, 5);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (4, 6, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (4, 7, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (4, 8, 1);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (5, 4, 8);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (6, 3, 14);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (6, 4, 4);
+INSERT INTO `ims`.`order_items` (`orderID`, `itemID`, `itemQuantity`) VALUES (7, 1, 7);
+
